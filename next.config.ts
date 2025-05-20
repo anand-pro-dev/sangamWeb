f35137604b3229fa4d7c3d0e0cdabbb1aff17e88
anand-pro-dev <anand.pro.dev@gmail.com>
2025-05-20 15:14:58 +0530

    done

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+//  @type {import('next').NextConfig} 
+const nextConfig = {
+<<<<<<< HEAD
+  experimental: {
+    appDir: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+=======
+  // experimental: {
+  //   appDir: true,
+  // },
+  reactStrictMode: true,
+  // swcMinify: true,
+>>>>>>> e6bde8bacfd9c6055a41a4f0768bf629581eb26e
+};
+
+module.exports = nextConfig;
+
+
+
