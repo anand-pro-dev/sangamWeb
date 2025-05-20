b76dd123ea72e851395b45ce83e1603a0bb14f77
salasar-anand <145999616+salasar-anand@users.noreply.github.com>
2025-05-20 15:25:41 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,10 @@
-//  @type {import('next').NextConfig} 
+// @type {import('next').NextConfig}
 const nextConfig = {
-<<<<<<< HEAD
   experimental: {
     appDir: true,
   },
   reactStrictMode: true,
   swcMinify: true,
-=======
-  // experimental: {
-  //   appDir: true,
-  // },
-  reactStrictMode: true,
-  // swcMinify: true,
->>>>>>> e6bde8bacfd9c6055a41a4f0768bf629581eb26e
 };
 
 module.exports = nextConfig;
-
-
-
